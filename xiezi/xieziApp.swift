946275c0327418054a1c61e7f8c40bb40b94123d
shigedangao <marc.inthaamnouay@gmail.com>
2022-09-25 22:23:49 +0200

    Initial Commit

---
+++ xiezi/xieziApp.swift
@@ -0,0 +1,17 @@
+//
+//  xieziApp.swift
+//  xiezi
+//
+//  Created by Marc Intha on 25/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct xieziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
